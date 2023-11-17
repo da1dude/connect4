4955d8a6d076b51fd4bc20788d818ae38b5c9d87
Paul K <pkerschieter@gmail.com>
2023-11-17 15:16:54 -0600

    more changes

--- js/main.js
+++ js/main.js
@@ -88,10 +88,10 @@ function renderBoard() {
             const cellId = `c${colIdx}r${rowIdx}`
             // I could have done this, but it's a lil clunky and old school
             // const cellId2 = 'c' + colIdx + 'r' + rowIdx
-            console.log('cellId', cellId)
+            // console.log('cellId', cellId)
 
             const cellEl = document.getElementById(cellId)
-            console.log('cellEl', cellEl)
+            // console.log('cellEl', cellEl)
 
             cellEl.style.backgroundColor = colors[cellVal]
             
@@ -149,13 +149,111 @@ function render() {
 }
 
 // handleDrop -> this will be the main gameplay function, finds the marker that was clicked on, and drops to the bottommost position allowed
+function handleDrop(event) {
+    // check if a move is valid
+    // determine the column selected
+    const colIdx = markerEls.indexOf(event.target)
+    console.log('this is colIdx inside handleDrop', colIdx)
+    // assign a value to a specific cell (board element) by updating the board array
+    const colArr = board[colIdx]
+    console.log('this is colArr inside handleDrop', colArr)
+    // we want to select the first row cell that we find in the colArr
+    // this gets the first item that contains a zero
+    // returns the item if true, -1 if false
+    const rowIdx = colArr.indexOf(0)
+    // if the move is invalid, exit the function so the user can try again
+    if (rowIdx === -1) return
+    // if the move IS valid, update the value of the board array
+    // we'll assign the value to the cell based on the turn
+    colArr[rowIdx] = turn
+    // after everything is done, change whose turn it is
+    turn *= -1
+    // after every move, we want to check for a winner
+    // we're going to assign the winning value to the winner variable
+    winner = getWinner(colIdx, rowIdx)
+    // after every move, we want to render the changes
+    render()
+    // console.log('this is the current board', board)
+}
 
-// getWinner -> checks to see if a player has won the game
+
 // we might need multiple functions to achieve this
+// helper function for our winchecking functions
+function countAdjacent(colIdx, rowIdx, colOffset, rowOffset) {
+    // get the most recently played color
+    const player = board[colIdx][rowIdx]
+    // count how many matches are adjacent
+    let count = 0
+
+    // use a while loop to check the spaces around the most recently played spot
+    colIdx += colOffset
+    rowIdx += rowOffset
+
+    while (
+        board[colIdx] !== undefined && 
+        board[colIdx][rowIdx] !== undefined && 
+        board[colIdx][rowIdx] === player
+    ) {
+        count++
+        colIdx += colOffset
+        rowIdx += rowOffset
+    }
+    console.log('the count in countAdjacent', count)
+
+    return count
+}
+// checkHorizontalWin
+function checkHorizontalWin(colIdx, rowIdx) {
+    // going to the left
+    const adjCountLeft = countAdjacent(colIdx, rowIdx, -1, 0)
+    // going to the right
+    const adjCountRight = countAdjacent(colIdx, rowIdx, 1, 0)
+
+    return adjCountLeft + adjCountRight >= 3 ? board[colIdx][rowIdx] : null
+}
+// checkVerticalWin
+function checkVerticalWinner(colIdx, rowIdx) {
+    // go from N to S
+    // 0 = not changing column
+    // -1 moves south down the column
+    return countAdjacent(colIdx, rowIdx, 0, -1) === 3 ? board[colIdx][rowIdx] : null
+}
+// checkDiagonalSENWWin
+function checkDiagonalSENWWin(colIdx, rowIdx) {
+    // go NW
+    const adjCountNW = countAdjacent(colIdx, rowIdx, -1, 1)
+    // go SE
+    const adjCountSE = countAdjacent(colIdx, rowIdx, 1, -1)
+
+    return adjCountNW + adjCountSE >= 3 ? board[colIdx][rowIdx] : null
+}
+// checkDiagonalSWNEWin
+function checkDiagonalSWNEWin(colIdx, rowIdx) {
+    // go NE
+    const adjCountNE = countAdjacent(colIdx, rowIdx, 1, 1)
+    // go SW
+    const adjCountSW = countAdjacent(colIdx, rowIdx, -1, -1)
+
+    return adjCountNE + adjCountSW >= 3 ? board[colIdx][rowIdx] : null
+}
+// getWinner -> checks to see if a player has won the game
+function getWinner(colIdx, rowIdx) {
+    console.log('this is colIdx - getWinner', colIdx)
+    console.log('this is rowIdx - getWinner', rowIdx)
+    console.log('the board', board)
+    return(
+        checkHorizontalWin(colIdx, rowIdx) ||
+        checkVerticalWinner(colIdx, rowIdx) ||
+        checkDiagonalSENWWin(colIdx, rowIdx) ||
+        checkDiagonalSWNEWin(colIdx, rowIdx)
+    )
+}
 
 //////////////////////////////////
 // event listeners
 //////////////////////////////////
 // what events will happen, what they should be attached to, and what functions they call
 // click on a marker (to make a move)
+document.getElementById('markers').addEventListener('click', handleDrop)
 // click play again (initialize an empty board) (reset all variables)
+playAgainButton.addEventListener('click', init)
