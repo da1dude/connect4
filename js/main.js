cf2c5eeeeb279a8ff4bb162530db0cf7615477ca
Paul K <pkerschieter@gmail.com>
2023-11-17 12:49:25 -0600

    first updates

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-console.log("Working!");
+// game displays rows and columns of circles
+//player is notified of turns
+//player click a button to make a move
+//the players move is displayed on the board
+//the turn switches from p1 to p2
+//p2 can select a move with the same process
+//at the end of the game announce the winner
+//allow the oppertunity to play again
+
+/*--------------------------------*/
+/*----- constants -----*/
+/*--------------------------------*/
+
+/*--------------------------------*/
+/*----- state variables -----*/
+/*--------------------------------*/
+
+/*--------------------------------*/
+/*----- cached elements  -----*/
+/*--------------------------------*/
+
+/*--------------------------------*/
+/*----- event listeners -----*/
+/*--------------------------------*/
+
+/*--------------------------------*/
+/*----- functions -----*/
+/*--------------------------------*/
