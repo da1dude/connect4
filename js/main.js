63e37fd80603ed18f7b7ba0159eed26faa033e6d
Paul K <pkerschieter@gmail.com>
2023-11-17 14:01:34 -0600

    more changes

--- js/main.js
+++ js/main.js
@@ -1,28 +1,161 @@
 // game displays rows and columns of circles
-//player is notified of turns
-//player click a button to make a move
-//the players move is displayed on the board
-//the turn switches from p1 to p2
-//p2 can select a move with the same process
-//at the end of the game announce the winner
-//allow the oppertunity to play again
+// player is notified of whose turn it is
+// player clicks a button to make a move
+// the player's move is displayed on the board
+// the turn switches from p1 to p2
+// p2 can select a move with the same process
+// at the end of a game, announce the winner
+// allow the opportunity to play again
 
-/*--------------------------------*/
-/*----- constants -----*/
-/*--------------------------------*/
+//////////////////////////////////
+// constants
+//////////////////////////////////
+// this section is going to hold things like players, colors, and anything that might be useful to refer to but doesn't change over the course of gameplay.
+// the only constant we're going to use here are the colors, stored in an object for the sake of easy reference.
+// so, player 1 has one color, player 2 a different color, or we'll initialize the game board pieces with a 'blank' color
+const colors = {
+    0: 'white',
+    1: 'purple',
+    // if we want to use a negative number for a key
+    // we can use a string
+    '-1': 'orange'
+}
 
-/*--------------------------------*/
-/*----- state variables -----*/
-/*--------------------------------*/
+//////////////////////////////////
+// state variables
+//////////////////////////////////
+// things that we want to constantly check, refer to, and change.
+// these will allow us to properly render changes at the right time.
+let board // an array of 7 nested arrays
+let turn // will be a value of 1 or -1 (1 || -1)
+let winner // null || 1 || -1 || 'T'
 
-/*--------------------------------*/
-/*----- cached elements  -----*/
-/*--------------------------------*/
+//////////////////////////////////
+// cached DOM elements
+//////////////////////////////////
+// grab our HTML elements, save them to variables, and use later
+const messageEl = document.querySelector('h2')
+const playAgainButton = document.querySelector('button')
+// we want to grab our marker elements and save them to an array
+// NOT A NODELIST
+// ... = spread operator
+// this operator takes a copy of whatever is selected(object, a nodelist, htmlcollection, array) and does something to all of the items
+// because we used array brackets, the spread operator is grabbing the items in the nodelist and pushing them into a new array
+const markerEls = [...document.querySelectorAll('#markers > div')]
+console.log('markerEls \n', markerEls)
 
-/*--------------------------------*/
-/*----- event listeners -----*/
-/*--------------------------------*/
+//////////////////////////////////
+// functions
+//////////////////////////////////
 
-/*--------------------------------*/
-/*----- functions -----*/
-/*--------------------------------*/
+// function - init - initializes an empty game.
+// the init function runs one time when the page loads
+// then the init function will be called again from the play again button
+
+function init() {
+    // set values for our state variables
+    turn = 1
+    winner = null
+
+    board = [
+        [0, 0, 0, 0, 0, 0], // col 0
+        [0, 0, 0, 0, 0, 0], // col 1
+        [0, 0, 0, 0, 0, 0], // col 2
+        [0, 0, 0, 0, 0, 0], // col 3
+        [0, 0, 0, 0, 0, 0], // col 4
+        [0, 0, 0, 0, 0, 0], // col 5
+        [0, 0, 0, 0, 0, 0], // col 6
+    ]
+
+    // CALL THE RENDER FUNCTION ONCE THE RENDER FUNCTION IS BUILT
+    render()
+}
+
+init()
+
+// function - renderBoard - render the game board
+function renderBoard() {
+    // loop over our array that represents the board
+    // apply a background color for each element
+    board.forEach((colArr, colIdx) => {
+        // colArr is the column, colIdx is the id within the array
+        // console.log('colArr', colArr)
+        // console.log('colIdx', colIdx)
+        colArr.forEach((cellVal, rowIdx) => {
+            // console.log('cellVal', cellVal)
+            // console.log('rowIdx', rowIdx)
+            // determine the id of the element
+            const cellId = `c${colIdx}r${rowIdx}`
+            // I could have done this, but it's a lil clunky and old school
+            // const cellId2 = 'c' + colIdx + 'r' + rowIdx
+            console.log('cellId', cellId)
+
+            const cellEl = document.getElementById(cellId)
+            console.log('cellEl', cellEl)
+
+            cellEl.style.backgroundColor = colors[cellVal]
+            
+        })
+    })
+}
+
+// render controls -> changes the visibility of the play again button
+function renderControls() {
+    // change initial vis of the playAgain button
+    // this uses a ternary operator
+    // ask a question ? if true, do this : if false do that
+    playAgainButton.style.visibility = winner ? 'visible' : 'hidden'
+    // change vis of our marker buttons
+    markerEls.forEach((markerEl, colIdx) => {
+        // if all board spaces are full (no 0's left) (means a tie)
+        // OR if we have a winner (winner is a truthy value (not null))
+        const hideMarker = !board[colIdx].includes(0) || winner
+        // if either of those conditions is truthy, hide the markers
+        // otherwise play can continue
+        markerEl.style.visibility = hideMarker ? 'hidden' : 'visible'
+    })
+}
+
+// render message -> display whose turn it is
+function renderMessage() {
+    // message a tie
+    if (winner === 'T') {
+        messageEl.innerText = "It's a Tie!!!!"
+    // message a winner
+    } else if (winner) {
+        messageEl.innerHTML = `
+            <span style="color: ${colors[winner]}">
+                ${colors[winner].toUpperCase()}
+            </span> Wins!
+        `
+    // or the current turn
+    } else {
+        messageEl.innerHTML = `
+            <span style="color: ${colors[turn]}">
+                ${colors[turn].toUpperCase()}
+            </span>'s Turn!
+        `
+    }
+}
+
+// render -> call all of our render based functions at once
+function render() {
+    // call renderBoard
+    renderBoard()
+    // call renderMessage
+    renderMessage()
+    // call renderControls
+    renderControls()
+}
+
+// handleDrop -> this will be the main gameplay function, finds the marker that was clicked on, and drops to the bottommost position allowed
+
+// getWinner -> checks to see if a player has won the game
+// we might need multiple functions to achieve this
+
+//////////////////////////////////
+// event listeners
+//////////////////////////////////
+// what events will happen, what they should be attached to, and what functions they call
+// click on a marker (to make a move)
+// click play again (initialize an empty board) (reset all variables)
